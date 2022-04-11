9b50c2363ef062ddb9cd2e5da0ae51692407104c
Jordan <bretton.jordan@gmail.com>
2022-04-11 14:40:24 +0200

    Change port and fix export service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -10,5 +10,6 @@ import { User, UserSchema } from './schema/users.schema';
     imports: [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ],
+    exports: [UsersService],
 })
 export class UsersModule {}
